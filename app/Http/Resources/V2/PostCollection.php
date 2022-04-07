2e497fdbae99647941a71a701b443e01a1b37bd0
Miguel <vivasmiguel96@gmail.com>
2022-04-06 22:01:57 -0500

    primer commit

---
+++ app/Http/Resources/V2/PostCollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\V2;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+    //en laravel 8 no es necesario usar esta linea
+    //public $collects = PostResource::class;
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Platzi',
+                'authors' => [
+                    'Italomoralesf',
+                    'Platzi'
+                ]
+                ],
+                'type' => 'articles'
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+
+//V1
+Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)
+    ->only(['index','show', 'destroy'])
+    ->middleware('auth:sanctum');;
+
+    //V2
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['index','show',])
+    ->middleware('auth:sanctum');
+
+Route::post('login',[
+     App\Http\Controllers\Api\LoginController::class,
+        'login'
+    ]);    
